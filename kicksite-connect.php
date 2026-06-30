a15c04aa61bf3c9cfcba8ddbb955d93b7c1b1274
Dustin Rangel <90423017+dustinKicksite@users.noreply.github.com>
2026-06-30 08:58:37 -0500

    Create kicksite-connect.php

---
+++ kicksite-connect.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Kicksite Connect
+ * Description: Connects your WordPress site to the Kicksite platform.
+ * Version:     1.0.0
+ * Author:      Kicksite
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
